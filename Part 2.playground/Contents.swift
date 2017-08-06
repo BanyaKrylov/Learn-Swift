253829d84b79fa20b7b47094d0d21c48a83d7b47
BanyaKrylov <ivan8532@me.com>
2017-08-06 11:56:41 +0300

    App2

---
+++ Part 2.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var namee: String?
+
+var num1 = 5
+var num2 = 5
+
+if num1 > num2 {
+    print("\(num1) > \(num2)")
+} else if num1 < num2 {
+    print("\(num1) < \(num2)")
+} else {
+    print("\(num1) = \(num2)")
+}
+
+if let <#constant name#> = <#optional#> {
+    <#statements#>
+}
