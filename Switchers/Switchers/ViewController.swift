1798c01d6ca8723e461754b32016b50a35518189
BanyaKrylov <ivan8532@me.com>
2017-08-09 17:14:53 +0300

    Промежуточный вариант1

--- Switchers/Switchers/ViewController.swift
+++ Switchers/Switchers/ViewController.swift
@@ -10,13 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
     var result: Int8 = 0
+    var randomTask: Int8 = Int8(arc4random_uniform(32))
     
     @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var task: UILabel!
     @IBOutlet weak var switch1: UISwitch!
     @IBOutlet weak var switch2: UISwitch!
     @IBOutlet weak var switch4: UISwitch!
     @IBOutlet weak var switch8: UISwitch!
     @IBOutlet weak var switch16: UISwitch!
+    
+    func getTasks() {
+        task.text = "Set num : \(String(randomTask))"
+    }
+    
+    func checkResult() {
+        if result == randomTask {
+            task.text = "You win!"
+        } else {
+            task.text = "Game over"
+        }
+    }
+    
+    @IBAction func startGame(_ sender: UIButton) {
+        getTasks()
+        switch1.setOn(false, animated: true)
+        switch2.setOn(false, animated: true)
+        switch4.setOn(false, animated: true)
+        switch8.setOn(false, animated: true)
+        switch16.setOn(false, animated: true)
+    }
+    
+    @IBAction func stopGame(_ sender: UIButton) {
+        checkResult()
+    }
     
     @IBAction func switch1(_ sender: UISwitch) {
         if sender.isOn {
@@ -28,7 +55,6 @@ class ViewController: UIViewController {
             result -= 1
             resultLabel.text = String(result)
         }
-        switch2(sender)
     }
     
     @IBAction func switch2(_ sender: UISwitch) {
@@ -52,7 +78,6 @@ class ViewController: UIViewController {
             result -= 4
             resultLabel.text = String(result)
         }
-        switch1(sender)
     }
     @IBAction func switch8(_ sender: UISwitch) {
         if sender.isOn {
@@ -75,10 +100,8 @@ class ViewController: UIViewController {
             result -= 16
             resultLabel.text = String(result)
         }
-        switch1(sender)
-        switch4(sender)
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
