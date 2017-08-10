25ace1bda59a4353b735e0aa8e44bb4b382dc597
BanyaKrylov <ivan8532@me.com>
2017-08-10 15:16:14 +0300

    Рефакторинг

--- Switchers/Switchers/ViewController.swift
+++ Switchers/Switchers/ViewController.swift
@@ -9,10 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var result: Int8 = 0
-    var randomTask: Int8 = Int8(arc4random_uniform(32))
+    var result = 0
+    var randomTask = 0
     
-    @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var task: UILabel!
     @IBOutlet weak var switch1: UISwitch!
     @IBOutlet weak var switch2: UISwitch!
@@ -20,19 +19,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var switch8: UISwitch!
     @IBOutlet weak var switch16: UISwitch!
     
-    func getTasks() {
-        task.text = "Set num : \(String(randomTask))"
+    // Рандомайзер
+    func generateRandomNum(){
+        randomTask = Int(arc4random_uniform(32))
     }
     
+    // Задание
+    func getTasks() {
+        task.text = "Set binary number = \(randomTask)"
+    }
+    
+    // Проверка и вывод результата
     func checkResult() {
         if result == randomTask {
             task.text = "You win!"
+            result = 0
         } else {
             task.text = "Game over"
+            result = 0
+        }
+    }
+    
+    // Управление перелкючателями + подсчет промежуточного рузультата
+    func switchDriver(sender: UISwitch, switcher: UISwitch, num: Int) {
+        if sender.isOn {
+            switcher.onImage = #imageLiteral(resourceName: "switch")
+            switcher.setOn(true, animated: true)
+            result += num
+        } else {
+            switcher.offImage = #imageLiteral(resourceName: "switch")
+            switcher.setOn(false, animated: true)
+            result -= num
         }
     }
     
     @IBAction func startGame(_ sender: UIButton) {
+        generateRandomNum()
         getTasks()
         switch1.setOn(false, animated: true)
         switch2.setOn(false, animated: true)
@@ -46,62 +68,21 @@ class ViewController: UIViewController {
     }
     
     @IBAction func switch1(_ sender: UISwitch) {
-        if sender.isOn {
-            switch1.setOn(true, animated: true)
-            result += 1
-            resultLabel.text = String(result)
-        } else {
-            switch1.setOn(false, animated: true)
-            result -= 1
-            resultLabel.text = String(result)
-        }
+        switchDriver(sender: sender, switcher: switch1, num: 1)
     }
     
     @IBAction func switch2(_ sender: UISwitch) {
-        if sender.isOn {
-            switch2.setOn(true, animated: true)
-            result += 2
-            resultLabel.text = String(result)
-        } else {
-            switch2.setOn(false, animated: true)
-            result -= 2
-            resultLabel.text = String(result)
-        }
+        switchDriver(sender: sender, switcher: switch2, num: 2)
     }
     @IBAction func switch4(_ sender: UISwitch) {
-        if sender.isOn {
-            switch4.setOn(true, animated: true)
-            result += 4
-            resultLabel.text = String(result)
-        } else {
-            switch4.setOn(false, animated: true)
-            result -= 4
-            resultLabel.text = String(result)
-        }
+        switchDriver(sender: sender, switcher: switch4, num: 4)
     }
     @IBAction func switch8(_ sender: UISwitch) {
-        if sender.isOn {
-            switch8.setOn(true, animated: true)
-            result += 8
-            resultLabel.text = String(result)
-        } else {
-            switch8.setOn(false, animated: true)
-            result -= 8
-            resultLabel.text = String(result)
-        }
+        switchDriver(sender: sender, switcher: switch8, num: 8)
     }
-    @IBAction func switch16(_ sender: UISwitch) {
-        if sender.isOn {
-            switch16.setOn(true, animated: true)
-            result += 16
-            resultLabel.text = String(result)
-        } else {
-            switch16.setOn(false, animated: true)
-            result -= 16
-            resultLabel.text = String(result)
-        }
+    @IBAction func switch16(_ sender: UISwitch) {switchDriver(sender: sender, switcher: switch16, num: 16)
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -111,7 +92,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
 }
+
 
