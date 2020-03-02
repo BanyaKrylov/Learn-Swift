8c699f27027daf262359295a56d5aae3475a5232
Ivan <ivan8532@me.com>
2020-03-02 15:34:22 +0300

    Init weather App

---
+++ Skill/Weather/Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Ivan Krylov on 02.03.2020.
+//  Copyright © 2020 Ivan Krylov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
